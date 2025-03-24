3fd9a8db4e6f074600822448dd620990f7a6a640
NigelFinegan5p <fvunpiqh@students.codeinstitute.net>
2025-03-24 16:04:11 +0000

    add stripe to base.html, add js & stripe elements.js file

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Qw1MQHBSjol7KXaluWHfU7apiTG6SKjpP0BnR6uux0KTsXQvgPaGr2E1RQkR13vvdnAqKpcFB2sJJeWeQaAepk200Kllx2CcT',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
