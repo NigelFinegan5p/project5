d6cfebe360b20bde7a4c6def4592728606248f47
NigelFinegan5p <fvunpiqh@students.codeinstitute.net>
2025-03-29 11:27:39 +0000

    finiashed add product functionality for add-new prodiuct - site owner

--- products/views.py
+++ products/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -70,6 +72,7 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+
 def add_product(request):
     """ Add a product to the store """
     if request.method == 'POST':
@@ -89,5 +92,4 @@ def add_product(request):
     }
 
     return render(request, template, context)
-
 
