e661b9a0488c043d387652820c34d82da51ad32e
NigelFinegan5p <fvunpiqh@students.codeinstitute.net>
2025-04-04 18:10:29 +0100

    update admin and products

--- coders/settings.py
+++ coders/settings.py
@@ -229,3 +229,5 @@ DEFAULT_FROM_EMAIL = 'codersroasthouse@example.com'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
