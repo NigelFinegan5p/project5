6ca1b3312f891fb8ad0c56af8cf718e587589a27
NigelFinegan5p <fvunpiqh@students.codeinstitute.net>
2025-04-09 12:34:14 +0100

    flake 8 - linespace, whitespace

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404  # noqa: E501 
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404  # noqa:E501
 from django.contrib import messages
 
 from products.models import Product
@@ -28,17 +28,17 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}') # noqa: E501
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}') # noqa:E501
             else:
                 bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')  # noqa: E501
+                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')  # noqa:E501
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'Added size {size.upper()} {product.name} to your bag')  # noqa: E501
+            messages.success(request, f'Added size {size.upper()} {product.name} to your bag')  # noqa:E501
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity 
-            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')  # noqa: E01
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')  # noqa:E501
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
@@ -101,8 +101,10 @@ def remove_from_bag(request, item_id):
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
-  
+
     except Exception as e:
 
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
+    
+    
