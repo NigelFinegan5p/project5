c92380b73b2ebf6ce8f535903327cd3eb01eb3af
NigelFinegan5p <fvunpiqh@students.codeinstitute.net>
2025-04-12 15:14:25 +0100

    newslatter app - update views and test

--- newsletter/views.py
+++ newsletter/views.py
@@ -4,9 +4,9 @@ from .forms import SubscriptionForm
 from .models import Subscriber, Newsletter
 from django.core.mail import send_mail
 from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
-from django.template.loader import render_to_string
 
+from django.core.mail import send_mail
+from django.conf import settings
 
 def subscribe(request):
     if request.method == 'POST':
@@ -14,24 +14,21 @@ def subscribe(request):
         if form.is_valid():
             subscriber = form.save()
 
-            subject = 'Subscription Confirmation'
-            from_email = settings.DEFAULT_FROM_EMAIL
-            to_email = subscriber.email
-
-            # Text & HTML content
-            text_content = 'Thank you for subscribing to our newsletter.'
-            html_content = render_to_string('newsletter/subscription_confirmation_email.html')
-
-            # Build and send the email
-            email = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
-            email.attach_alternative(html_content, "text/html")
-            email.send()
+            # Send confirmation email
+            send_mail(
+                subject='Subscription Confirmation',
+                message='Thank you for subscribing to our newsletter!',
+                from_email=settings.DEFAULT_FROM_EMAIL,
+                recipient_list=[subscriber.email],
+                fail_silently=False,
+            )
 
             messages.success(request, 'Thank you for subscribing! A confirmation email has been sent.')
             return redirect('subscribe_success')
     else:
         form = SubscriptionForm()
     return render(request, 'newsletter/subscribe.html', {'form': form})
+
 
 
 
